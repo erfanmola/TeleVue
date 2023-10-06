ef1b3e5e08c5c9c14095b0bacba81679a0f58bc4
Erfan Mola <hi@erfanxd.ir>
2023-10-07 01:21:26 +0330

    Added Checkbox

--- src/stories/List.vue
+++ src/stories/List.vue
@@ -1,5 +1,6 @@
 <script setup>
     import List from '../components/List.vue';
+    import Checkbox from '../components/Checkbox.vue';
 
     const props = defineProps({
         platform: {
@@ -16,8 +17,15 @@
 
 <template>
     <List v-bind="props">
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
+        <li><Checkbox :checked="true" />Item 1</li>
+        <li><Checkbox :checked="true" />Item 2</li>
+        <li><Checkbox :checked="true" />Item 3</li>
     </List>
 </template>
+
+<style lang="scss" scoped>
+    li {
+        display: flex;
+        gap: 1rem;
+    }
+</style>
